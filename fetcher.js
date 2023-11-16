cd866c82334236746c505dd92c24dbe5d74fc82e
Matt Morgan <matt.james.morgan97@gmail.com>
2023-11-16 03:28:08 +0000

    fixed async issues and changed filecheck function

--- fetcher.js
+++ fetcher.js
@@ -1,55 +1,69 @@
 const request = require('request');
 const fs = require('fs');
-const { error } = require('console');
 const options = process.argv.slice(2);
 
 const printResults = (bytes, file) =>{
-  console.log(`Downloaded and saved ${bytes} bytes to ${file}`)
-}
+  console.log(`Downloaded and saved ${bytes} bytes to ${file}`);
+};
 
-const fileCheck = (newFile) => {
+const fileCheck = (newFile, callback) => {
+  
   fs.readdir("./", (err, files)=>{
-    if(err){
-      console.log(err);
-    }else{
-      files.forEach(file=>{
-        if(file === newFile){
-          return false
-        }
-      })
+    if (files.includes(newFile)) {
+      callback(false);
+    } else {
+      callback(true);
     }
-  })
-  return true;
-}
+  });
+  
+};
 
-function readAndWrite(print, checkFile){
-  return request(`${options[0]}`, (error, repsonse, body)=>{
-  try{
-    if(error){
-      throw new Error("Server Error")
-    }
-    if(repsonse.statusCode === 404){
-      throw new Error("Not Found");
-    }
-    if(!options[0] || !options[1]){
-      throw new Error("Need two inputs");
-    }
-    if(!checkFile(options[1])){
-      throw new Error("File already exists");
-    }
-    
-  }catch(error){
-    console.log("Error: ", error.message);
+const readAndWrite = (print, fileCheck) => {
+  if (!options[0] || !options[1]) {
+    console.log("Two inputs needed");
     return;
   }
-  
-  
-  
-  print(repsonse.headers['content-length'], options[1]);
-})
-}
+
+  fileCheck(options[1], (valid) => {
+
+    if (!valid) {
+      console.log("File already exists");
+      return;
+    }
+
+    return request(`${options[0]}`, (error, repsonse, body)=>{
+
+      try {
+
+        if (error) {
+          throw new Error("Server Error");
+        }
+
+        if (repsonse.statusCode === 404) {
+          throw new Error("Not Found");
+        }
+          
+      } catch (error) {
+        console.log("Error: ", error.message);
+        return;
+      }
+        
+      fs.writeFile(options[1], body, "utf8", (err)=>{
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+        
+      print(repsonse.headers['content-length'], options[1]);
+    });
+
+  });
+      
+};
+    
 
 
 
-readAndWrite(printResults);
+readAndWrite(printResults, fileCheck);
 
